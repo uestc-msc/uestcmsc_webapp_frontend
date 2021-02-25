bf6be26fed48b9eca05ea3042ed5820baf19ae1d
lyh543 <lyh543@outlook.com>
2021-02-25 14:45:23 +0800

    更改后端部署域名为 api.uestc-msc.com

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 const { verbose_name, version } = require('/package.json');
 const DEBUG = process.env.NODE_ENV === 'development';
-const baseUrl = DEBUG ? 'http://localhost:8000/api' : 'https://uestcmsc-webapp.lyh543.cn/api';
+const baseUrl = DEBUG ? 'http://localhost:8000/api' : 'https://api.uestc-msc.com/api';
 const debounceTime = 500;
 
 export {
