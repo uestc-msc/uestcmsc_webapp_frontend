60533c271c3909e2b6f9f54cf44cc9d55c22150f
lyh543 <lyh543@outlook.com>
2021-04-26 22:34:04 +0800

    修复 PeopleChipGroup 对不齐的问题

--- src/components/ui/base/people-chip-group.vue
+++ src/components/ui/base/people-chip-group.vue
@@ -1,7 +1,8 @@
 <!--  封装好的 UserChipGroup  -->
 <template>
   <v-chip-group
-    :class=" listitem ? 'chip-group-in-list' :'' "
+    class="people-chip-group"
+    :class="listitem ? 'people-chip-group-as-list-item' : ''"
     column
   >
     <PeopleChip
@@ -13,14 +14,18 @@
 </template>
 
 <style scoped>
-/* TODO: 我爬了 两个场景上下对不齐也太难顶了 */
 /* https://vue-loader.vuejs.org/zh/guide/scoped-css.html#深度作用选择器 */
-.chip-group-in-list >>> .v-slide-group__content {
+.people-chip-group >>> .v-slide-group__content {
   padding: 0;
 }
 
-.chip-group-in-list >>> .v-chip {
-  margin: 0 8px 0 0;
+.people-chip-group >>> .v-chip {
+  margin: 2px 8px 2px 0;
+}
+
+/* 在 list-item 里的 chip-group，其 margin-top 应当对齐 list-item 的 padding-top */
+.people-chip-group-as-list-item >>> .v-chip {
+  margin: 0 8px 2px 0;
 }
 </style>
 
